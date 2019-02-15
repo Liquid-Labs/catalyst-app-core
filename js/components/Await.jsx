e6c0cb297c8907ef3b3f747ea6d458a338614337
Zane Rockenbaugh <zane@liquid-labs.com>
2019-02-15 16:35:13 -0600

    sketch in updated catalyst aware Await

--- js/components/Await.jsx
+++ js/components/Await.jsx
@@ -1,4 +1,32 @@
-import { Await as AwaitBase } from '@liquid-labs/react-await'
+import React from 'react'
 
-const Await = (props) =>
-  <AwaitBase spinner={CenteredProgress} blocked={() => null} {...props} />
+import * as appActions from '../actions/appActions'
+import { connect } from 'react-redux'
+
+import { Await as AwaitBase, defaultReportDisplay } from '@liquid-labs/react-await'
+import { Centered } from '@liquid-labs/mui-extensions'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import ErrorIcon from '@material-ui/icons/Error'
+
+const DefaultSpinner = (report) =>
+<Centered method="flex">
+  <CircularProgress /><br />
+  { defaultReportDisplay(report) }
+</Centered>
+
+const DefaultBlocked = (report) =>
+  <Centered method="flex">
+    <ErrorIcon fontSize="large" /><br />
+    { defaultReportDisplay(report) }
+  </Centered>
+
+const mapDispatchToProps = (dispatch) => ({
+  followupHandler : (msg) => dispatch(appActions.setInfoMessage(msg))
+})
+
+const Await = connect(null, mapDispatchToProps)(
+  (props) =>
+    <AwaitBase spinner={DefaultSpinner} blocked={DefaultBlocked} {...props} />
+)
+
+export { Await }
