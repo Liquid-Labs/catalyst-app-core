7c94a7777b7dd020d43d4f27feadd6ed38ea68e7
Zane Rockenbaugh <zane@liquid-labs.com>
2019-03-29 10:23:29 -0500

    added TODO with issue ref

--- js/components/layouts/BasicContentFrame.jsx
+++ js/components/layouts/BasicContentFrame.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 import { AppMain } from './AppMain'
 import { AppNavigation } from './AppNavigation'
 
-// TODO: handle 'navLogoTo' in 'AppNavigation' via settings. This is not
-// something we need to handle changing constantly.
+// TODO https://github.com/Liquid-Labs/catalyst-core-ui/issues/4
 const BasicContentFrame = ({children, navChildren, navLogoTo, navRightChildren, navShowChildren, AppNavigationProps, ...remainder}) => [
   <AppNavigation key="appNavigation"
       showChildren={navShowChildren}
