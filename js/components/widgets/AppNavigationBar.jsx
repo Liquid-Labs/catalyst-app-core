4b78127f33ee7a07ae40e08e5dc5305c6574e273
Zane Rockenbaugh <zane@liquid-labs.com>
2019-04-24 08:42:06 -0500

    relocated
    should have been moved, but separated from the the remove

---
+++ js/components/widgets/AppNavigationBar.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+
+import BottomNavigation from '@material-ui/core/BottomNavigation'
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
+
+import { makeStyles, useTheme } from '@material-ui/styles'
+
+import classNames from 'classnames'
+
+
+const style = (theme) => ({
+  root: {
+    width: '100%',
+    bottom: 0,
+    borderTop: '2px solid',
+    borderColor: theme.palette.primary.main,
+    '&$shrinkSome > *': {
+      [theme.breakpoints.down('xs')]: {
+        minWidth: '65px',
+        '& span': {
+          fontSize: '0.75rem'
+        }
+      }
+    },
+    '&$shrinkMore > *': {
+      [theme.breakpoints.down('xs')]: {
+        minWidth: '55px',
+        '& span': {
+          fontSize: '0.70rem'
+        }
+      }
+    }
+  },
+  shrinkSome: {},
+  shrinkMore: {},
+
+  action: {
+    fontFamily: "'Open Sans'",
+    fontWeight: 700,
+    textTransform: "uppercase"
+  },
+})
+
+const useAppNavigationBarStyles = makeStyles(style)
+
+const AppNavigationBar = ({onChange, variant, showLabel=false}) => {
+  const [selection, setSelection] = useState('home')
+  const theme = useTheme()
+  const classes = useAppNavigationBarStyles()
+  const pathname = window.location.pathname
+
+  return (
+    <BottomNavigation value={1/*selection*/} onChange={onChange}
+        className={classNames(classes.root,
+          'small' === variant && classes.shrinkMore,
+          'tiny' === variant && classes.shrinkSome)}>
+      {
+        theme.layout.sections.map((sectionDef) => {
+          const { label, path, onClick, Icon } = sectionDef
+          return <BottomNavigationAction className={classes.action} key={path} label={label} showLabel={showLabel} icon={<Icon />} />
+        })
+      }
+    </BottomNavigation>
+  )
+}
+
+export { AppNavigationBar }
