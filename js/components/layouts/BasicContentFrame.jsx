2902f49c9c2c2c9243e76e18caaf03fa2984cfff
Zane Rockenbaugh <zane@liquid-labs.com>
2019-05-03 11:40:15 -0500

    remainder -> props

--- js/components/layouts/BasicContentFrame.jsx
+++ js/components/layouts/BasicContentFrame.jsx
@@ -2,21 +2,26 @@ import React from 'react'
 
 import { AppMain } from './AppMain'
 import { AppMenuBar } from '../widgets/AppMenuBar'
+import Grid from '@material-ui/core/Grid'
 
 import { useTheme } from '@material-ui/styles'
 
+import merge from 'lodash.merge'
+
 // TODO https://github.com/Liquid-Labs/catalyst-core-ui/issues/4
-const BasicContentFrame = ({children, AppMenuBarProps={}, ...remainder}) => {
+const BasicContentFrame = ({children, AppMenuBarProps={}, ...props}) => {
   const theme = useTheme()
   const menuHeight = theme.layout.header.variant === 'dense' ? 36 : 64
   const navHeight = theme.layout.navigation.visible ? 56 : 0
   const ctrlsHeight = menuHeight + navHeight
 
+  merge(AppMenuBarProps, theme?.layout?.header?.props)
+
   return (<>
     <AppMenuBar {...AppMenuBarProps}>
       {AppMenuBarProps.children}
     </AppMenuBar>
-    <AppMain {...remainder} style={{ maxHeight: `calc(100vh - ${ctrlsHeight}px)`}}>
+    <AppMain {...props} style={{ maxHeight: `calc(100vh - ${ctrlsHeight}px)`}}>
       {children}
     </AppMain>
   </>
